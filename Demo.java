9706c98bc121a9d97a1ee8772accfefddcf8299f
saadullah bin ahmad-cs <saadullahbinahmad@gmail.com>
2026-09-16 15:23:04 +0500

    Update oop lab 2

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.id = 101;
+        s.name = "Alice";
+        s.completedCredits = 15;
+
+        s.printSummary();
+    }
+}
--- Student.java
+++ Student.java
@@ -1,25 +1,9 @@
 public class Student {
+    int id;
+    String name;
+    int completedCredits;
 
-    public static void main(String[] args) {
-
-        String name = "Saadullah";
-
-        int mark1 = 80;
-        int mark2 = 75;
-        int mark3 = 90;
-
-        int total = mark1 + mark2 + mark3;
-
-        double average = total / 3.0;
-
-        System.out.println("Student Name: " + name);
-        System.out.println("Total Marks: " + total);
-        System.out.println("Average: " + average);
-
-        if (average >= 50) {
-            System.out.println("Result: Pass");
-        } else {
-            System.out.println("Result: Fail");
-        }
+    void printSummary() {
+        System.out.println(id + " " + name + " " + completedCredits);
     }
 }
